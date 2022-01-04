19f1e0eb8e1fe4d8fce18e26e61ebcc07fe3a9ae
ishwar <jagadi1994@gmail.com>
2022-01-04 17:19:24 +0530

    cab

---
+++ CAB/src/test/java/OLA/controller/TestCucumber.java
@@ -0,0 +1,15 @@
+package OLA.controller;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.CucumberOptions.SnippetType;
+
+/**
+ *
+ * @author Kent Yeh
+ */
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber-reports/cucumber.html", "json:target/cucumber-reports/cucumber.json"}, snippets = SnippetType.CAMELCASE,
+        features = "src/test/resources/features", glue = "OLA.cucumber")
+public class TestCucumber extends AbstractTestNGCucumberTests {
+
+}
